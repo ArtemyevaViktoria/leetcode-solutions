cc41aaae463a8c021b3510f4e3dffa7822e5ace7
Artemyeva Viktoria <v.i.artemyeva@yandex.ru>
2024-05-01 19:05:51 +0300

    Time: 67 ms (21.41%), Space: 50.8 MB (93.61%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    const mergeArr = [];
+
+    for(let i = 0; i < n; i++) {
+        mergeArr.push(nums[i], nums[n+i]);
+    }
+
+    return mergeArr;
+};
