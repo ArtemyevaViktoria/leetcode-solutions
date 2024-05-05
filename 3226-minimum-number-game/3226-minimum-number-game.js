24a6881bb83a357f3af82ef8098190b1312b2615
Artemyeva Viktoria <v.i.artemyeva@yandex.ru>
2024-05-05 20:25:22 +0300

    Time: 84 ms (15.51%), Space: 52.2 MB (44.05%) - LeetHub

---
+++ 3226-minimum-number-game/3226-minimum-number-game.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var numberGame = function(nums) {
+    let arr = [];
+
+    var nums = nums.sort((a, b) => b - a);
+
+    while (nums.length) {
+        let alice = nums.pop();
+        let bob = nums.pop();
+
+        arr.push(bob);
+        arr.push(alice);
+    }
+
+    return arr; 
+};
