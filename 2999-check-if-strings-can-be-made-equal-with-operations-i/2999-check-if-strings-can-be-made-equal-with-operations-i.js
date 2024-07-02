1d78714ab5a740540e27ca2738015713361554ab
Artemyeva Viktoria <v.i.artemyeva@yandex.ru>
2024-07-02 13:03:41 +0300

    Time: 75 ms (57.14%), Space: 52.6 MB (69.39%) - LeetHub

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/2999-check-if-strings-can-be-made-equal-with-operations-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var canBeEqual = function(s1, s2) {
+    for(let i = 0; i < 2; i++) {
+        if(s1[i] === s2[i] && s1[i+2] === s2[i+2] || s1[i] === s2[i+2] && s2[i] === s1[i+2]) {
+            continue;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+};
